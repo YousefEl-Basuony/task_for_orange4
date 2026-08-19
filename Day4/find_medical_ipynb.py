ae7eac401e54783d228045f24750fa24b94c35b5
Yousef Mohamed El-Basuony <120418668+YousefEl-Basuony@users.noreply.github.com>
2026-08-20 00:09:05 +0300

    Final

--- Day4/find_medical_ipynb.py
+++
@@ -1,5 +0,0 @@
-import os
-for root, dirs, files in os.walk(r"c:\Users\DELL\Desktop\medical-system"):
-    for file in files:
-        if file.endswith(".ipynb"):
-            print(os.path.join(root, file))
--- Day4/find_task4.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for root, dirs, files in os.walk(r"c:\Users\DELL\Desktop"):
-    for file in files:
-        if "task4" in file.lower() or "safety" in file.lower():
-            print(os.path.join(root, file))
